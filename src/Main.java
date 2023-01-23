997b3198c1be45b15869cc9e2407dbe88acb3adb
Yo231s <dawid.lempicki.1@gmail.com>
2023-01-23 18:21:23 +0100

    proyectos

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
 public class Main {
         public static void main(String[] args) {
             System.out.println("Products");
-            Products products1 = new Products("botella", 1, 8.0, "botella de plástico");
-            Products products2 = new Products("vaso",2,2.50,"vaso de cartón");
-            Products products3 = new Products("tenedor",3,5.50,"tenedor de plata");
-            Products products4 = new Products("cuchillo",4,6.0,"cuchillo de plata");
-            Products products5 = new Products("cuchara",5, 5.50,"cuchara de plata");
+            Products products1 = new Products("agua", 1, 1.0, "botella de plástico");
+            Products products2 = new Products("pan",2,2.50,"plastico");
+            Products products3 = new Products("carne",3,5.50,"bolsa envuelta");
+            Products products4 = new Products("manzanas",4,1.0,"bolsa plastico");
+            Products products5 = new Products("pescado",5, 5.50,"bolsa envuelta");
         Products[] arrayproducts = new Products[]{products1,products2,products3,products4,products5};
         System.out.println(products1);
         System.out.println(products2);
